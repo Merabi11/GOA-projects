7ae25e08a9bcdce40a849886025f626236927249
Merabi11 <Llama12.Llama12@gmail.com>
2025-02-20 23:20:23 +0400

    day 01 homework commit

--- day 01/homework/homework1.py
+++ day 01/homework/homework1.py
@@ -1,1 +1,19 @@
-print("No Homework")
+
+#string (str - სთრინგ ტიპის)
+#integer (int - რიცხვი)
+#float (არამთელი რიცხვი)
+#boolean (bool - ბულიანი მაგ: true and false)
+#type (არჩევს თუ რა ტიპის არის მნიშვნელობა)
+
+
+name = "merab"
+
+last_name = "jiqidze"
+
+age = 20
+
+my_heigth = 1.79
+
+my_hobby = "programing"
+
+print(f"my name is: {name}  / my last name is: {last_name} / my age is: {age} / my heigth is: {my_heigth} / my hobby is: {my_hobby}" )
