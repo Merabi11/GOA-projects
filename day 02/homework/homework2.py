40d457d68b510b2d6296081bda2bf148e9261061
Merabi11 <Llama12.Llama12@gmail.com>
2025-02-27 20:41:00 +0400

    commit day 02 classwork and homework

---
+++ day 02/homework/homework2.py
@@ -0,0 +1,23 @@
+#homework 1
+
+num1 = 20
+
+num2 = 20
+
+total = num1 + num2
+
+print(f"ჯამი: {total}")
+
+#homework 2
+
+age = int(input("შეიყვანეთ თქვენი ასაკი:"))
+
+if age >= 18:
+    print("თქვენ სრულწლოვანი ხართ ")
+
+elif age < 18: 
+    print("თქვენ არასრულწლოვანი ხართ")
+
+else:
+    print("თქვენ ასაკი არ გიწყობთ ხელს")
+
