50eb50132d224ce06affd568d648ca54e72fc62f
Merabi11 <Llama12.Llama12@gmail.com>
2025-03-05 22:27:13 +0400

    commit day 05 classwork and homework

---
+++ day 05/classwork/classwork5.py
@@ -0,0 +1,58 @@
+
+#classwork-1
+
+#შევქმენი ცვლადი ამ ცვლადში შევინახე ინფუთი ამ იმფუთში შეყვანილი first_name შეინახება ამავე ცვლადში ანუ (first_name)-ში.
+first_name = input("enter your fist name: ")
+
+
+#შევქმენი ცვლადი ამ ცვლადში შევინახე ინფუთი ამ იმფუთში შეყვანილი last_name შეინახება ამავე ცვლადში ანუ (last_name)-ში.
+last_name = input("enter your last name: ")
+
+
+#შევქმენი ცვლადი ამ ცვლადში შევინახე ინფუთი ან იმფუთში შეყვანილი age შეინახება ამავე ცვლადში ანუ (age)-ში.
+age = input("enter your age: ")
+
+
+#first_name input - შეყვანილი ნებისმიერი სახელს დაგვიპრინტავს ნებისმიერ შეყვანილ სახელე გვეტყვის რომ (your name is: ....)
+print("your name is: " + first_name)
+
+
+#last_name input - შეყვანილი ნებისმიერი გვარს დაგვიპრინტავს ნებისმიერ შეყვანილ გვარზე გვეტყვის რომ (your last name is: ....)
+print("your last name is: " + last_name)
+
+
+#age input - შეყვანილი ნებისმიერი ასაკს დაგვიპრინტავს ნებისმიერ შეყვანილ ასაკზე გვეტყვის რომ (your age is: ....)
+print("your age is: " + age)
+
+
+#classwork-2
+
+num1 = int(input("enter first number: "))
+
+num2 = int(input("enter second number: "))
+
+print(num1 + num2)
+print(num1 - num2)
+print(num1 * num2)
+print(num1 / num2)
+print(num1 % num2)
+
+#classwork-3 (BONUS)
+
+first_name2 = input("please enter your name: ")
+
+age2 = int(input("please enter your age: "))
+
+
+if age2 >= 18:
+    print("თქვენ შეგიძლიათ კლუბში შესვლა.")
+
+elif age2 < 18:
+    print("სამწუხაროდ თქვენ არ შეგიძლიათ კლუბში შესვლა რადგან არ იმყოფებით სრულწლოვანი.")
+
+
+
+    
+
+
+
