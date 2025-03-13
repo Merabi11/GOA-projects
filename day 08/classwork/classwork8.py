353f814328c65a6b7852d609ce2e2ff0cf81f490
Merabi11 <Llama12.Llama12@gmail.com>
2025-03-13 20:04:25 +0400

    commit, day 08 classwork and homework

---
+++ day 08/classwork/classwork8.py
@@ -0,0 +1,3 @@
+num = int(input("Enter number:"))
+
+print(num >= 5 and num <= 10)
---
+++ day 08/homework/homework8.py
@@ -0,0 +1,40 @@
+
+#homework - 1
+
+print(True and False or False or True and False) #False
+
+
+
+#homework - 2 
+
+num1 = int(input("Enter the number:"))
+
+if num1 % 2 == 0 and num1 > 10:
+    print(True)
+
+else:
+    print(False)
+
+
+#homework - 3
+
+#for loop - მაშინ გამოვიყენეთ თუ ვიცით რამდენჯერ უნდა გამოვიყენოთ კოდი ანუ განსაზღრული რაოდენობით მაგალითად რამდენჯერ გვინდა გამოვატანინოტ კოდი
+
+for num2 in range(1, 16):
+    print(num2)
+
+
+
+#homework - 4
+
+numbers = 0
+
+for num3 in range(1, 10):
+    numbers += num3
+    print(f"ჯამი: {numbers}")
+
+
+
+#homework - 5
+
+print("Sololearn - შესრულებულია.")
