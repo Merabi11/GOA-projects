db5dff7dbdf965ada91e36ff2bcf8e279d4f6317
Merabi11 <Llama12.Llama12@gmail.com>
2025-03-16 18:55:18 +0400

    commit, day 10 classwork and homework

---
+++ day 10/classwork/classwork10.py
@@ -0,0 +1,10 @@
+user = int(input("Please enter number: "))
+
+password = 117
+
+while user != password:
+    print("პაროლი არასწორია შეიყვანეთ სწორი პაროლი!")
+if user == password:
+    print("პაროლი სწორია.")
+        
+
