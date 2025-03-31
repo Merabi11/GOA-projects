aa5e1ab39231b687d92333e580807191328852cb
Merabi11 <Llama12.Llama12@gmail.com>
2025-03-31 18:33:27 +0400

    commit, day 15 classwork and homework

---
+++ day 15/homework/homework15.py
@@ -0,0 +1,24 @@
+#homework - 1
+
+user_input = input("Please enter your names: ")
+
+print(f"Uppercase: {user_input.upper()}")
+print(f"Lowercase: {user_input.lower()}")
+print(f"Capitalized: {user_input.capitalize()}")
+
+
+#homework - 2
+
+def username(names):
+    return names.upper(), names.lower(), names.capitalize() 
+
+print(username(input("Please enter your names: ")))
+
+#homework - 3
+
+print("Sololearn - შესრულებულია")
+
+
+#homework - 4
+
+print("Codewars - შესრულებულია")
