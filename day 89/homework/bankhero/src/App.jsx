672180a6139ce13c71b07a29f0a59ef4a9eed4a1
Merabi11 <Llama12.Llama12@gmail.com>
2025-09-29 00:42:19 +0400

    commit, day 89 classwork and homework

--- day 89/homework/bankhero/src/App.jsx
+++ day 89/homework/bankhero/src/App.jsx
@@ -1,25 +1,7 @@
 function App() {
   return (
     <>
-      <div>
-        <a>
-          <img />
-        </a>
-        <a>
-          <img />
-        </a>
-      </div>
-
-      <h1>Vite + React</h1>
-
-      <div>
-        <button>count is {count}</button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-
-      <p>Click on the Vite and React logos to learn more</p>
+      <h1>პროექტის კეთების პროცესი</h1>
     </>
   );
 }
